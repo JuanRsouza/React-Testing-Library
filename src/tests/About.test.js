dd50743dd76b3128dd89742e7a0061ddde08826e
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-11-22 17:09:01 -0300

    Req 2 Finalizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test(' ', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Teste o componente <About.js />.', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    const p1 = screen.getByText(
+      /this application simulates a pokédex, a digital encyclopedia containing all pokémon/i,
+    );
+    const p2 = screen.getByText(
+      /one can filter pokémon by type, and see more details for each one of them/i,
+    );
+    const img = screen.getByRole('img', {
+      name: /pokédex/i,
+      src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    });
+    expect(heading).toBeInTheDocument();
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
